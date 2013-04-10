585ff6ebb0a11512beba6eaee347ee7e82f0725c
Nick Pack <nick@nickpack.com>
2013-04-10 15:42:05 +0100

    Initial simple version

---
+++ example/core.make
@@ -0,0 +1,10 @@
+api = 2
+core = 7.x
+
+projects[drupal][type] = core
+projects[drupal][version] = 7.21
+projects[ctools] = 1.3
+projects[weight] = 2.2
+projects[entity] = 1.0
+projects[views] = 3.7
+projects[token] = 1.5
--- tasks/drush.js
+++ tasks/drush.js
@@ -1,10 +1,50 @@
-'use strict';
+/*
+ * grunt-drush
+ * https://github.com/nickpack/grunt-drush
+ *
+ * Copyright (c) 2013 Nick Pack
+ * Licensed under the MIT license.
+ */
 
-module.exports = function(grunt) {
-  
-  var fs = require('fs');
+ 'use strict';
 
-  grunt.registerMultiTask('drush', 'Drush runner', function(context) {
-    var config = this,
+ module.exports = function(grunt) {
+
+  var fs   = require('fs'),
+  path = require('path');
+  var _ = grunt.util._;
+  var helpers = require('grunt-lib-contrib').init(grunt);
+  var self = this;
+
+  grunt.registerMultiTask('drush', 'Drush task runner for grunt.', function() {
+    var cb = this.async();
+    var options = this.options();
+    var args;
+
+    grunt.verbose.writeflags(options, 'Options');
+
+    grunt.util.async.forEachSeries(this.files, function(f, next) {
+      args = [].concat(f.args);
+
+      if (f.dest !== 'undefined') {
+        args.push(f.dest);
+      }
+
+      var drush = grunt.util.spawn({
+        cmd: 'drush',
+        args: args
+      }, function(error, result, code) {
+        if (code === 127) {
+          return grunt.warn(
+            'You need to have drush installed and in your PATH for\n' +
+            'this task to work.'
+            );
+        }
+        next(error);
+      });
+
+      drush.stdout.pipe(process.stdout);
+      drush.stderr.pipe(process.stderr);
+    }, cb);
   });
-}
+};
