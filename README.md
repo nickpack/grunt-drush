8fad2a2b76a8457a43670d3036673729846d826b
Nick Pack <nick@nickpack.com>
2013-04-10 11:38:00 +0100

    Stubbing

---
+++ README.md
@@ -0,0 +1,7 @@
+# Grunt Drush Task
+
+## About
+Run [Drush](http://www.drush.org/) tasks from grunt.
+
+## Changelog
+
---
+++ tasks/drush.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function(grunt) {
+  
+  var fs = require('fs');
+
+  grunt.registerMultiTask('drush', 'Drush runner', function(context) {
+    var config = this,
+  });
+}
